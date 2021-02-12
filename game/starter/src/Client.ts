3d19cd82136172ac31f04749593a2df6e108c0ee
Andrew Munro <synestry@gmail.com>
2021-02-12 22:11:03 +0000

    Updating to latest ECS engine

--- game/starter/src/Client.ts
+++ game/starter/src/Client.ts
@@ -1,16 +1,14 @@
-import { Entity } from '@ecs/core/Entity';
 import { useQueries } from '@ecs/core/helpers';
-import { all } from '@ecs/core/Query';
-import { System } from '@ecs/core/System';
 import TickerEngine from '@ecs/core/TickerEngine';
 import Transform from '@ecs/plugins/math/Transform';
 import Vector3 from '@ecs/plugins/math/Vector';
 import { LoadPixiAssets } from '@ecs/plugins/render/2d/helpers/PixiHelper';
 import PixiRenderSystem from '@ecs/plugins/render/2d/systems/PixiRenderSystem';
 import { Container, Point, Sprite, TilingSprite } from 'pixi.js';
+import { all, Entity, System } from 'tick-knock';
 
 const ScreenSize = new Vector3(1280, 720);
-const ScreenCenter = ScreenSize.multiF(0.5);
+const ScreenCenter = ScreenSize.multi(0.5);
 
 const Assets = {
     BACKGROUND_PATTERN: "pattern.png",
@@ -36,19 +34,9 @@ export class Engine extends TickerEngine {
         background.add(container);
         this.addEntities(background);
 
-        const logo = new Entity();
-        logo.add(Transform, {
-            scale: Vector3.EQUAL(0.6),
-            position: ScreenSize.sub(new Vector3(20, 20))
-        });
-        logo.add(Sprite.from(Assets.LOGO), {
-            anchor: new Point(1, 1)
-        });
-        this.addEntities(logo);
-
         const box = new Entity();
         box.add(Transform, {
-            scale: Vector3.EQUAL(0.5),
+            scale: Vector3.Equal(0.5),
             position: ScreenCenter.clone()
         });
         box.add(Sprite.from(Assets.BOX), {
@@ -56,6 +44,16 @@ export class Engine extends TickerEngine {
         });
         box.add(RotationMovement);
         this.addEntities(box);
+
+        const logo = new Entity();
+        logo.add(Transform, {
+            scale: Vector3.Equal(0.6),
+            position: ScreenSize.sub(new Vector3(20, 20))
+        });
+        logo.add(Sprite.from(Assets.LOGO), {
+            anchor: new Point(1, 1)
+        });
+        this.addEntities(logo);
     }
 }
 
